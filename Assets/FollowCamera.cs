aff1b13aa86199b35b934eceec4213a073475fa1
Thieu Huy Nguyen <86967333+NguyenThieuHuy@users.noreply.github.com>
2023-07-30 17:14:10 +0900

    First commit

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] GameObject thingToFollow;
+    // this things position (camera) should be the same as the car's position
+
+    void LateUpdate()
+    {
+        transform.position = thingToFollow.transform.position + new Vector3 (0,0,-10);
+    }
+}
